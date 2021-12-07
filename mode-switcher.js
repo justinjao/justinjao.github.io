1280602b4ff286e5a7eb81cc63d3b8a1384b8f0c
justinjao <jao.c.justin@gmail.com>
2021-12-06 22:59:51 -0800

    fixed dark mode toggling, cleaned up code, and added comments for future self just in case

--- mode-switcher.js
+++ mode-switcher.js
@@ -1,3 +1,6 @@
+// this checks to see if prefers dark theme is on (i.e. if it is nighttime)
+// and it also checks to see whether there is a session storage of 'theme'
+// (i.e. if you're visiting another page in the same session)
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
 let theme = sessionStorage.getItem('theme');
 
@@ -7,6 +10,11 @@ let theme = sessionStorage.getItem('theme');
 // 	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 // }
 
+// this section runs if there is already a session storage set, and is necessary
+// to ensure that if a user toggles the theme to manually override the system preferences
+// that it will carry over as they visit other pages. Tbh, not sure why this is necessary
+// to be inserted here, as I thought that this same block of code (at the end) performs this check
+// but for some reason, if I remove this section here, it stops carrying over. So yeah, it needs it.
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
@@ -17,7 +25,9 @@ if (theme === "dark") {
 	// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
 
-
+// this section is used to declare a function that checks whether system preferences
+// change (e.g. if you're looking during the day and are still on the site as it turns to night)
+// if so, it sete the attribute to dark, and clears the current session storage
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
@@ -29,9 +39,14 @@ function prefersColorTest(systemInitiatedDark) {
     sessionStorage.setItem('theme', '');
   }
 }
+
+// this is the event listener that checks if the previous function value changes
 systemInitiatedDark.addEventListener(prefersColorTest);
 
 
+// this next function controls what happens when the toggle button is clicked
+// when thinking through this, remember that whatever the theme is currently set to,
+// we want the OPPOSITE to happen when we toggle the switch
 function modeSwitcher() {
 	let theme = sessionStorage.getItem('theme');
 	if (theme === "dark") {
